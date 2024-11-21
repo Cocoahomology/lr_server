a10891827448d4700824a48b050116e6d579f017
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2024-11-22 04:32:08 +0800

    fix: add validator to CryptoCurrency
    This validator ensures that the last_updated field is always in the past when an entry is added to the db.

--- price_app/models.py
+++ price_app/models.py
@@ -1,17 +1,32 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+import datetime
+
 
 # TODO: Verify price precision meets requirements. max_digits and decimal_places were arbitrarily selected.
-"""FIXME: A token is not uniquely identified by its name and symbol, it is uniquely identified by its contract address and chain.
-To increase robustness, the model should be updated to include the contract address and chain as fields that are unique_together.
-"""
+# FIXME: A token is not uniquely identified by its name and symbol, it is uniquely identified by its contract address and chain.
+# To increase robustness, the model should be updated to include the contract address and chain as fields that are unique_together.
 # TODO: Add more constraints/warnings for suspected erroneous prices.
+# TODO: Add more validation for ALL fields in model.
+
+
+def validate_past_date(value):
+    if value.tzinfo is None:
+        value = value.replace(tzinfo=datetime.timezone.utc)
+    if value > timezone.now():
+        raise ValidationError("The date cannot be in the future.")
 
 
 class CryptoCurrency(models.Model):
     name = models.CharField(max_length=100)
     symbol = models.CharField(max_length=10)
     price = models.DecimalField(max_digits=24, decimal_places=8)
-    last_updated = models.DateTimeField()
+    last_updated = models.DateTimeField(validators=[validate_past_date])
+
+    def save(self, *args, **kwargs):
+        self.full_clean()  # This will call all field validators
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.name} ({self.symbol})"
