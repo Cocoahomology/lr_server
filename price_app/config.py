2147ee008b56def6926e8b38b6b034904eaedb32
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2024-11-22 02:04:59 +0800

    feat: update CryptoCurrency task inserts into db
    Basic logic for update_cryptocurrency_prices task to insert requested data into the db.

--- price_app/config.py
+++ price_app/config.py
@@ -1,7 +1,13 @@
 # Description: Configuration file for which tokens to fetch data for from DefiLlama API.
+# Also contains a mapping for token chain/address to name, which determines 'name' field in CryptoCurrency model.
 # TODO: Ensure file is in sensible place (need to research best practices for configuration files in Django projects).
 
 TOKEN_ADDRESS_LIST = [
     "bsc:0x762539b45a1dcce3d36d080f74d1aed37844b878",
     "ethereum:0xdB25f211AB05b1c97D595516F45794528a807ad8",
 ]
+
+TOKEN_NAME_MAPPING = {
+    "bsc:0x762539b45a1dcce3d36d080f74d1aed37844b878": "Linear",
+    "ethereum:0xdB25f211AB05b1c97D595516F45794528a807ad8": "Stasis Euro",
+}
--- price_app/tasks.py
+++ price_app/tasks.py
@@ -1,16 +1,58 @@
+from .models import CryptoCurrency
 from celery import shared_task
 from .services.defillama_request_service import defillama_request_service
-import logging
 from . import config
+from datetime import datetime, timezone
+import logging
 
 logger = logging.getLogger(__name__)
 
 
 @shared_task(bind=True)
 def update_cryptocurrency_prices(self):
-    response = defillama_request_service.get_cryptocurrency_prices_from_defillama(
-        config.TOKEN_ADDRESS_LIST, 90
-    )
-    # TODO: insert some retry logic here for failed requests.
-    # TODO: insert response into DB
+    # TODO: Add retry logic for failed requests
+    # TODO: Consider better error handling
+    # TODO: Ensure the insertion is being done atomically (need to double-check Django ORM behavior)
+    # TODO: Consider moving this logic to separate module.
+    try:
+        response_json = (
+            defillama_request_service.get_cryptocurrency_prices_from_defillama(
+                config.TOKEN_ADDRESS_LIST, 5
+            )
+        )
+        token_data_dict = response_json.get("coins")
+        if not token_data_dict:
+            logger.error("No token data found in the response.")
+            return
+    except Exception as e:
+        logger.error(f"Error fetching cryptocurrency prices: {e}")
+        return
+
+    for token, token_data in token_data_dict.items():
+        try:
+            token_name = config.TOKEN_NAME_MAPPING.get(token)
+            token_symbol = token_data["symbol"]
+            token_price = token_data["price"]
+            token_last_updated = datetime.fromtimestamp(
+                timestamp=int(token_data["timestamp"]),
+                tz=timezone.utc,
+            )
+        except KeyError as e:
+            logger.error(f"Missing expected key in token data: {e}")
+            continue
+
+        try:
+            CryptoCurrency.objects.update_or_create(
+                name=token_name,
+                symbol=token_symbol,
+                defaults={
+                    "price": token_price,
+                    "last_updated": token_last_updated,
+                },
+            )
+        except Exception as e:
+            logger.error(
+                f"Error updating or creating CryptoCurrency object for token {token_name}: {e}"
+            )
+
     return
