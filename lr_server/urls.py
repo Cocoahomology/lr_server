fa8fd540819656628c0e3dee0e7adf238cb248ab
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2024-11-22 17:02:24 +0800

    fix: missing response for home path

--- lr_server/urls.py
+++ lr_server/urls.py
@@ -17,10 +17,20 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 from graphene_django.views import GraphQLView
 from .schema import schema
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse(
+        '<a href="/admin/">Admin</a><br><a href="/graphql/">GraphQL</a>'
+    )
+
 
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("graphql/", GraphQLView.as_view(graphiql=True, schema=schema)),
 ]
