86aa57076b626416bd33edd25242a4b0ca5afd56
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2024-11-23 22:05:47 +0800

    fix: typo in entrypoint.sh script

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ python manage.py migrate
 
 # Create a superuser with username and password 'admin' if it doesn't exist
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
-    python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').exists() or User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')"
+    python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username=\"$DJANGO_SUPERUSER_USERNAME\").exists() or User.objects.create_superuser(\"$DJANGO_SUPERUSER_USERNAME\", \"$DJANGO_SUPERUSER_EMAIL\", \"$DJANGO_SUPERUSER_PASSWORD\")"
 fi
 
 # Execute the command passed to the script
